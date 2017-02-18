1f71b006b3e1c2b2546b99a066bd1d115ba7aec7
tengxing <tengxing7452@163.com>
2017-02-19 02:59:42 +0800

    修正二进制数据读取制作工具

--- python_importData/yy.py
+++ python_importData/yy.py
@@ -16,6 +16,7 @@ def create_record():
             img = Image.open(img_path)
             img = img.resize((64, 64))
             img_raw = img.tobytes() #将图片转化为原生bytes
+            print index,img_raw
             example = tf.train.Example(
                features=tf.train.Features(feature={
                     "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
@@ -29,7 +30,7 @@ data = create_record()
 #读取二进制数据
 def read_and_decode(filename):
     # 创建文件队列,不限读取的数量
-    filename_queue = tf.train.string_input_producer([filename],shuffle=False)
+    filename_queue = tf.train.string_input_producer([filename])
     # create a reader from file queue
     reader = tf.TFRecordReader()
     # reader从文件队列中读入一个序列化的样本
@@ -56,15 +57,15 @@ if __name__ == '__main__':
         data = create_record("train.tfrecords")
     else:
         img, label = read_and_decode("train.tfrecords")
-        # print label
-        # print img
-        # 使用shuffle_batch可以随机打乱输入 next_batch挨着往下取
+        print label
+        print img
+        #使用shuffle_batch可以随机打乱输入 next_batch挨着往下取
         # shuffle_batch才能实现[img,label]的同步,也即特征和label的同步,不然可能输入的特征和label不匹配
         # 比如只有这样使用,才能使img和label一一对应,每次提取一个image和对应的label
         # shuffle_batch返回的值就是RandomShuffleQueue.dequeue_many()的结果
         # Shuffle_batch构建了一个RandomShuffleQueue，并不断地把单个的[img,label],送入队列中
         img_batch, label_batch = tf.train.shuffle_batch([img, label],
-                                                    batch_size=5, capacity=2000,
+                                                    batch_size=2, capacity=2000,
                                                     min_after_dequeue=1000)
         #print img_batch.shape,label_batch
 
@@ -73,26 +74,9 @@ if __name__ == '__main__':
 
         with tf.Session() as sess:
             sess.run(init)
-            # 创建一个协调器，管理线程
-            coord = tf.train.Coordinator()
             # 启动队列
-            threads = tf.train.start_queue_runners(coord=coord)
+            threads = tf.train.start_queue_runners(sess=sess)
             for i in range(3):
                 val, l = sess.run([img_batch, label_batch])
                 # l = to_categorical(l, 12)
-                print img_batch.eval()#将字符串str当成有效Python表达式来求值，并返回计算结果。
                 print(val.shape, l)
-                coord.request_stop()
-            '''
-            try:
-                while not coord.should_stop():
-                    e_val, l_val = sess.run([img_batch, label_batch])
-                    print e_val, l_val
-            except tf.errors.OutOfRangeError:
-                print('Epochs Complete!')
-            finally:
-                coord.request_stop()
-                coord.join(threads)
-                coord.request_stop()
-                coord.join(threads)
-            '''
